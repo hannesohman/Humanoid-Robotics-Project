5b11fc4d84011aedfcf8cb89accb9be4ee863dc5
Hannes Öhman <hannesoh@chalmers.se>
2025-10-30 10:48:08 +0100

    Removed redundant comments

--- interface.py
+++ interface.py
@@ -2,12 +2,6 @@ import serial
 from serial.tools import list_ports
 import time
 
-# for port in list_ports.comports():
-#     print(port.pid)
-#     if port.pid == 67: # replace with the pid of your device
-#         print(port.name)
-#         ser = serial.Serial(port.name)
-
 # Open serial connection to Arduino
 arduino = serial.Serial(port='COM3', baudrate=9600, timeout=1)  # Change COM port as needed
 time.sleep(2)  # wait for Arduino reset
@@ -37,32 +31,5 @@ while True:
         arduino.write((command + "\n").encode())  # send command
 
 
-
 # A B F K
 # LMNOPQRSTUVWXYZÅ
-
-
-## ARDUINO CODE:
-
-# void loop() {
-#   if (Serial.available() > 0) {
-#     String command = Serial.readStringUntil('\n'); // read incoming text
-
-#     if (command.startsWith("MOVE1")) {
-#       int angle = command.substring(6).toInt();
-#       servo1.write(angle);
-#     } 
-#     else if (command.startsWith("MOVE2")) {
-#       int angle = command.substring(6).toInt();
-#       servo2.write(angle);
-#     } 
-#     else if (command == "WAVE") {
-#       for (int i = 0; i < 3; i++) {
-#         servo1.write(0);
-#         delay(300);
-#         servo1.write(90);
-#         delay(300);
-#       }
-#     }
-#   }
-# }
